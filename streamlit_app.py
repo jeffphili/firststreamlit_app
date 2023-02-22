a0d70908a8e04faf3c642b886cd7fe0eec70d5b2
jeffphili <125653923+jeffphili@users.noreply.github.com>
2023-02-22 14:11:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit
-streamlit.title("hey Hi jeffin");
+streamlit.title("My Parents new healthy dinner");
+
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.txt("🥣 🥗 🐔 🥑🍞')
+
+
